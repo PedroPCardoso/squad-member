61ac43521764d164780c5bd013b94d042efff4a4
Pedro Cardoso <pedroecomp@gmail.com>
2024-08-31 15:35:44 -0300

    Ajustes no docker para testes

--- backend/src/plans/plans.controller.ts
+++ backend/src/plans/plans.controller.ts
@@ -1,9 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { PlansService } from './plans.service';
+
 @Controller('plans')
 export class PlansController {
     constructor(private plansService: PlansService) { }
-    a
+
+    @Get()
     async getClients() {
         return this.plansService.getAllPlans();
     }
